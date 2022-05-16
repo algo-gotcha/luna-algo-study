a801c2af42c23d6f3492f9997d591beca13abb0f
Luna <luvbliny@gmail.com>
2022-05-16 19:07:49 +0900

    Feat: using hash 0x01 02

--- bkd/0x01/02.py
+++ bkd/0x01/02.py
@@ -1,8 +1,15 @@
+#def solution(arr, n):
+#	for i, a in enumerate(arr):
+#		for j, b in enumerate(arr):
+#			if i != j and a + b == 100:
+#				return 1
+#	return 0
+
 def solution(arr, n):
-	for i, a in enumerate(arr):
-		for j, b in enumerate(arr):
-			if i != j and a + b == 100:
-				return 1
+	numbers = [100 - n for n in range(101)]
+	for a in arr:
+		if numbers[a] != 50 and numbers[a] in arr:
+			return 1
 	return 0
 
 print(solution({1, 52, 48}, 3) == 1)
