6a55b94cbf48d178423b22ff0481150855be78e1
Luna <luvbliny@gmail.com>
2022-05-20 14:15:59 +0900

    bkd : 1406 solved

--- bkd/0x04/1406.py
+++ bkd/0x04/1406.py
@@ -1,24 +1,23 @@
 import sys
+from collections import deque
 
-string = list(sys.stdin.readline().rstrip())
-count = int(input())
-index = len(string) - 1
-max_index = index
+front = deque(sys.stdin.readline().rstrip())
+count = int(sys.stdin.readline())
+
+back = deque()
 
 for _ in range(count):
     command = sys.stdin.readline().rstrip()
     if command[0] == 'P':
-        string.insert(index + 1, command[-1])
-        index += 1
-    elif command[0] == 'B':
-        if index >= 0:
-            del string[index]
-            index -= 1
-    elif command[0] == 'L' and index >= 0:
-        index -= 1
-    elif command[0] == 'D' and max_index:
-        index += 1
+        front.append(command[-1])
+    elif command[0] == 'B' and len(front) > 0:
+        front.pop()
+    elif command[0] == 'L' and len(front) > 0:
+        back.appendleft(front.pop())
+    elif command[0] == 'D' and len(back) > 0:
+        front.append(back.popleft())
 
+string = front + back
 
 
 print()
