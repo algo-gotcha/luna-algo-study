d263f0aaf09550b2451f85e7e6700634fd4c37bf
Luna <luvbliny@gmail.com>
2022-05-20 13:48:19 +0900

    bkd 0x04

--- bkd/0x04/1406.py
+++ bkd/0x04/1406.py
@@ -1,74 +1,28 @@
-string = input()
+import sys
+
+string = list(sys.stdin.readline().rstrip())
 count = int(input())
+index = len(string) - 1
+max_index = index
 
-index = len(string)
-
-class Cursor:
-    def __init__(self, character=''):
-        self.next = None
-        self.prev = None
-        self.data = character
-
-class CursorManager:
-    def __init__(self, string=''):
-        self.head = None
-        self.tail = None
-        self.string = string
-        self.index = len(string)
-    
-    def get_head_n_tail(self):
-        prev = self.head
-        for s in self.string:
-            cursor = Cursor(s)
-            if prev:
-                prev.next = cursor
-            else:
-                self.head = cursor
-            cursor.prev = prev
-            prev = cursor
-        return self.head, cursor
-
-    def print(self):
-        while self.head:
-            print(self.head.data)
-            self.head = self.head.next
-
-    def get_left(self, cursor):
-        if cursor.prev:
-            cursor = cursor.prev
-            self.index -= 1
-    
-    def get_right(self, cursor):
-        if cursor.next:
-            cursor = cursor.next
-            self.index += 1
-    
-    def erase(self, cursor):
-        cursor.data = None
-        cursor.prev.next = cursor.prev.next.next
-        cursor.next.prev = cursor.next.prev.prev
-        cursor.prev = None
-        cursor.next = None
-    
-    def insert(self, cursor, key):
-        new_cursor = Cursor(key)
-        cursor.next.prev = new_cursor
-        new_cursor.next = cursor.next
-        cursor.next = new_cursor
-        new_cursor.prev = cursor
-        self.index += 1
-
-cursorM = CursorManager(string)
-cursorM.print()
-
-
-cursor = None
+for _ in range(count):
+    command = sys.stdin.readline().rstrip()
+    if command[0] == 'P':
+        string.insert(index + 1, command[-1])
+        index += 1
+    elif command[0] == 'B':
+        if index >= 0:
+            del string[index]
+            index -= 1
+    elif command[0] == 'L' and index >= 0:
+        index -= 1
+    elif command[0] == 'D' and max_index:
+        index += 1
 
 
 
-
-
- 
-print(string)
-
-
+print()
+print()
+print(''.join(string))
+print()
+print()
