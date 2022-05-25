d0c39d3eb39abeedc1f0f7e96f575b999e76ebe7
Luna <luvbliny@gmail.com>
2022-05-26 06:49:14 +0900

    boj silver unsolved

--- boj/class2/10989.py
+++ boj/class2/10989.py
@@ -1,8 +1,31 @@
 import sys
-
+from collections import deque
 
 loop = int(sys.stdin.readline())
 
-results = []
+front = deque()
+front.append(0)
+back = deque()
+back.appendleft(10000001)
 for _ in range(loop):
-	results.append(int(sys.stdin.readline()))
+    number = int(sys.stdin.readline())
+    if number < front[-1]:
+        while front and number < front[-1]:
+            back.appendleft(front.pop())
+    if back[0] >= number >= front[-1]:
+        front.append(number)
+        continue
+    if number > back[0]:
+        while back and number > back[0]:
+            front.append(back.popleft())
+    if number <= back[0]:
+        back.appendleft(number)
+        continue
+
+front.popleft()
+back.pop()
+print()
+for f in front:
+    print(f)
+for b in back:
+    print(b)
