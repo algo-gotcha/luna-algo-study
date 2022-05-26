b4ca99f24012ab1b03651b2e09efab05dbbccae6
Luna <luvbliny@gmail.com>
2022-05-26 15:59:58 +0900

    boj class2

--- boj/class2/10989.py
+++ boj/class2/10989.py
@@ -1,31 +1,14 @@
 import sys
 from collections import deque
 
-loop = int(sys.stdin.readline())
+loop = int(sys.stdin.readline().rstrip())
 
-front = deque()
-front.append(0)
-back = deque()
-back.appendleft(10000001)
+hash = [0 for i in range(10001)]
+
 for _ in range(loop):
-    number = int(sys.stdin.readline())
-    if number < front[-1]:
-        while front and number < front[-1]:
-            back.appendleft(front.pop())
-    if back[0] >= number >= front[-1]:
-        front.append(number)
-        continue
-    if number > back[0]:
-        while back and number > back[0]:
-            front.append(back.popleft())
-    if number <= back[0]:
-        back.appendleft(number)
-        continue
+    number = int(sys.stdin.readline().strip())
+    hash[number] += 1
 
-front.popleft()
-back.pop()
-print()
-for f in front:
-    print(f)
-for b in back:
-    print(b)
+for i, count in enumerate(hash):
+    for _ in range(count):
+        print(i)
