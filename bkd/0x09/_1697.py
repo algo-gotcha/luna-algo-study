1f95f425b0b4d72fcde69557488ce3360400744b
Luna <luvbliny@gmail.com>
2022-05-26 15:59:46 +0900

    bkd 0x09 bfs

--- bkd/0x09/_1697.py
+++ bkd/0x09/_1697.py
@@ -1,19 +1,19 @@
 import sys
 from collections import deque
 
-MAX = 10 ** 5
 N, K = map(int, sys.stdin.readline().split())
-visited = [0 for _ in range(MAX + 1)]
+
+MAX = 10 ** 5
 q = deque()
 q.append(N)
-visited[N] = 1
-count = 0
+visited = [0 for _ in range(MAX + 1)]
+
 while q:
     x = q.popleft()
     if x == K:
-        print(visited[x] - 1)
+        print(visited[x])
         break
     for nx in (x - 1, x + 1, x * 2):
         if 0 <= nx <= MAX and not visited[nx]:
-            q.append(nx)
             visited[nx] = visited[x] + 1
+            q.append(nx)
